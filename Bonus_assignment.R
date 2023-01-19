a0708064b352fc5a0f031e7f66a9cb81dba57cc7
Musa Azeem <musa.mazeem@gmail.com>
2023-01-18 21:17:48 -0500

    update

--- Bonus_assignment.R
+++ Bonus_assignment.R
@@ -1,24 +1,5 @@
-SizeOfS = factorial(10)
-
-SizeOfA = 9
-
-for (i in 1:9) {
-    # calculate x
-    x = 0
-    for (j in (10-(i-1)):10) {
-        x = x + j
-    }
-
-    SizeOfA = SizeOfA * (((10-i)*i/x + (10-i-1)*(x-1)/x))
-}
-
-P_A = SizeOfA/SizeOfS
-
-print(P_A)
-
-
 # Simulation
-N = 10000            # loop 1000 times
+N = 100000           # loop 1000 times
 occur_i = 0          # Number of times the correct sample is given
 
 for (i in 1:N) {
@@ -36,4 +17,5 @@ for (i in 1:N) {
 
 occur = N - occur_i
 P_A = occur/N
-print(P_A)
+cat(P_A*100, "%")
+# Output is about 37%
